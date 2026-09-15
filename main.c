d2e5d6c1d6addf5b205cf41fa3a58425c99b23dd
AlondraAgurto <alondra.agurto1@mail.udp.cl>
2026-09-15 20:42:27 -0300

    Separar variablesss

--- main.c
+++ main.c
@@ -13,6 +13,21 @@ int main(int argc, char *argv[]){
 
     while (fgets(buffer, sizeof(buffer), archivo) != NULL) {
         printf("Linea leida: %s", buffer);
+
+        /*
+            char* token = strtok(input, " ");
+            while (token)
+            {
+                puts(token);
+                token = strtok(NULL, " ");
+            }
+        */
+
+        char* id_actividad = strtok(buffer, ":");
+        char* nombre_actividad= strtok(NULL, ":");
+        char* tiempo_ms= strtok(NULL, ":");
+        char* dependencias= strtok(NULL, ":");
+        printf("ID: %s | Nombre: %s | Tiempo: %s | Dependencias: %s\n", id_actividad, nombre_actividad, tiempo_ms, dependencias);
     }
     
     // relleno xd
