3b1215e9f9245a330f4c871bac11d6d91ee0035e
AlondraAgurto <alondra.agurto1@mail.udp.cl>
2026-09-15 20:32:35 -0300

    while + leer archivo

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    FILE *archivo = fopen(argv[1], "r");;
+    // argv[1] toma el 1er argumento  al ejecutar programa:d
+    // r para leer 
+    // fopen para abrir
+    // *archivo es un puntero
+    FILE *archivo = fopen(argv[1], "r");
 
+    char buffer[256]; // arreglo caracteres
+
+    while (fgets(buffer, sizeof(buffer), archivo) != NULL) {
+        printf("Linea leida: %s", buffer);
+    }
+    
+    // relleno xd
+    fclose(archivo);
     return 0;
 }
